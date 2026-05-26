452b9403749bb1cb4233fbe2366a8d430e9913dd
Kevin Malakoff <kmalakoff@gmail.com>
2026-05-26 08:56:20 -0700

    Fix broken types

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import isAbsolute from 'is-absolute';
+// @ts-expect-error: no types for minimatch
 import minimatch from 'minimatch';
 import path from 'path-posix';
 import unixify from 'unixify';
@@ -13,7 +14,7 @@ export type Matcher = (filePath: string) => boolean;
 const isArray = Array.isArray || ((x) => Object.prototype.toString.call(x) === '[object Array]');
 
 // Case sensitivity: Windows/MSYS/Cygwin = insensitive, Unix = sensitive
-const isWindows = process.platform === 'win32' || /^(msys|cygwin)$/.test(process.env.OSTYPE);
+const isWindows = process.platform === 'win32' || /^(msys|cygwin)$/.test(process.env.OSTYPE ?? '');
 let nocase = isWindows;
 
 export function _setNocase(value: boolean): void {
@@ -23,7 +24,7 @@ export function _getNocase(): boolean {
   return nocase;
 }
 
-function stringStartsWith(string, check) {
+function stringStartsWith(string: string, check: string): boolean {
   if (nocase) {
     string = string.toLowerCase();
     check = check.toLowerCase();
@@ -36,11 +37,11 @@ export default function createMatcher(options: Options): Matcher {
   const include = options.include === undefined ? [] : options.include;
   const exclude = options.exclude === undefined ? [] : options.exclude;
 
-  function matchFn(condition) {
+  function matchFn(condition: string) {
     let pattern = unixify(condition);
     if (cwd && !isAbsolute(pattern) && pattern.indexOf('*') !== 0) pattern = path.join(cwd, pattern);
 
-    return function match(filePath) {
+    return function match(filePath: string) {
       return stringStartsWith(filePath, pattern) || minimatch(filePath, pattern, { nocase: nocase, dot: true });
     };
   }
@@ -48,7 +49,7 @@ export default function createMatcher(options: Options): Matcher {
   const includes = (isArray(include) ? (include as string[]) : (include as string).split(',')).map(matchFn);
   const excludes = (isArray(exclude) ? (exclude as string[]) : ((exclude as string) || '').split(',')).map(matchFn);
 
-  return function match(filePath) {
+  return function match(filePath: string) {
     filePath = unixify(filePath);
     for (let i = 0; i < excludes.length; ++i) {
       if (excludes[i](filePath)) return false;
---
+++ src/types/path-posix.d.ts
@@ -0,0 +1,4 @@
+declare module 'path-posix' {
+  import path from 'path';
+  export = path;
+}
